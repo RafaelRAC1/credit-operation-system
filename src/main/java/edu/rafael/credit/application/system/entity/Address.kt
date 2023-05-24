8f3175bcf51c8ea626072d00360aba657fac2d56
RafaelRAC1 <rafaelcorrea2154r@gmail.com>
2023-05-23 22:02:20 -0300

    model classes with JPA annotations

---
+++ src/main/java/edu/rafael/credit/application/system/entity/Address.kt
@@ -0,0 +1,10 @@
+package edu.rafael.credit.application.system.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Embeddable
+
+@Embeddable
+data class Address (
+    @Column(nullable = false) var zipCode: String = "",
+    @Column(nullable = false) var street: String = ""
+)
