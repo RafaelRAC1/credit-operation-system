ad6dda8d8c086c8c69e13d6510cfc8ff57f45e11
RafaelRAC1 <rafaelcorrea2154r@gmail.com>
2023-05-23 22:01:08 -0300

    initial commit

---
+++ src/test/java/edu/rafael/credit/application/system/CreditApplicationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.rafael.credit.application.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditApplicationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
