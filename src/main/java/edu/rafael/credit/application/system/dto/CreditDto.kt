c94a2d6a8ec2396072ed39a915dec5b1e1f7b6fd
RafaelRAC1 <rafaelcorrea2154r@gmail.com>
2023-05-25 14:49:08 -0300

    updating CreditDto annotations

--- src/main/java/edu/rafael/credit/application/system/dto/CreditDto.kt
+++ src/main/java/edu/rafael/credit/application/system/dto/CreditDto.kt
@@ -2,14 +2,17 @@ package edu.rafael.credit.application.system.dto
 
 import edu.rafael.credit.application.system.entity.Credit
 import edu.rafael.credit.application.system.entity.Customer
+import jakarta.validation.constraints.Future
+import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.NotNull
 import java.math.BigDecimal
 import java.time.LocalDate
 
 data class CreditDto (
-    val creditValue: BigDecimal,
-    val dayFirstOfInstallment: LocalDate,
+    @field:NotNull(message = "Invalid input") val creditValue: BigDecimal,
+    @field:Future(message = "Invalid input") val dayFirstOfInstallment: LocalDate,
     val numberOfInstallments: Int,
-    val customerId: Long
+    @field:NotNull(message = "Invalid input") val customerId: Long
 ) {
     fun toEntity(): Credit = Credit(
         creditValue = this.creditValue,
