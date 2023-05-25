763b67228c3be1b05cc3442ad402d8e0b9be0535
RafaelRAC1 <rafaelcorrea2154r@gmail.com>
2023-05-25 14:47:14 -0300

    update CostumerDto annotations

--- src/main/java/edu/rafael/credit/application/system/dto/CustomerDto.kt
+++ src/main/java/edu/rafael/credit/application/system/dto/CustomerDto.kt
@@ -2,17 +2,21 @@ package edu.rafael.credit.application.system.dto
 
 import edu.rafael.credit.application.system.entity.Address
 import edu.rafael.credit.application.system.entity.Customer
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.NotNull
+import org.hibernate.validator.constraints.br.CPF
 import java.math.BigDecimal
 
 data class CustomerDto (
-    val firstName: String,
-    val lastName: String,
-    val cpf: String,
-    val income: BigDecimal,
-    val email: String,
-    val password: String,
-    val zipCode: String,
-    val street: String
+    @field:NotEmpty(message = "Name field can't be empty") val firstName: String,
+    @field:NotEmpty(message = "Last Name field can't be empty") val lastName: String,
+    @field:CPF(message = "Invalid CPF") val cpf: String,
+    @field:NotNull(message = "Income field can't be empty") val income: BigDecimal,
+    @field:Email(message = "Invalid email") val email: String,
+    @field:NotEmpty(message = "Password field can't be empty") val password: String,
+    @field:NotEmpty(message = "Zip Code field can't be empty") val zipCode: String,
+    @field:NotEmpty(message = "Street field can't be empty") val street: String
 ) {
     fun toEntity(): Customer = Customer(
         firstName = this.firstName,
